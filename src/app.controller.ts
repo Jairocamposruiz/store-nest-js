da084ff5e4ec478260b78a004349da620743c930
Jairo Campos Ruiz <Jairocamposruiz1991@gmail.com>
2022-04-12 22:29:14 +0200

    Generating controllers, modules and services

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,31 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  //
+  // @Get('new')
+  // newEndpoint(): string {
+  //   return 'Endpoint nuevo';
+  // }
+  //
+  // @Get('products')
+  // getProducts(
+  //   @Query('limit') limit = 100,
+  //   @Query('offset') offset = 10,
+  //   @Query('brand') brand: string,
+  // ): string {
+  //   return `Productos limit = ${limit} | offset = ${offset} | brand = ${brand}`;
+  // }
+  //
+  // @Get('products/:id')
+  // getProduct(@Param('id') id: string): string {
+  //   return `Producto con id ${id}`;
+  // }
+  //
+  // @Get('categories/:id/products/:productId')
+  // getCategory(
+  //   @Param('id') id: string,
+  //   @Param('productId') productId: string,
+  // ): string {
+  //   return `Categoria con id ${id} y producto con id ${productId}`;
+  // }
 }
